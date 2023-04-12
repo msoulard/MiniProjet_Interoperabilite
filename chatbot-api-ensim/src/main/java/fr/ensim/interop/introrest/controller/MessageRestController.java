04c40a6e34b440f89f306278f02f2a80761e8387
msoulard <soulardmimi@gmail.com>
2023-04-12 11:26:54 +0200

    méthode getUpdates sans offset
    méthode du controller MessageRestController

--- chatbot-api-ensim/src/main/java/fr/ensim/interop/introrest/controller/MessageRestController.java
+++ chatbot-api-ensim/src/main/java/fr/ensim/interop/introrest/controller/MessageRestController.java
@@ -1,7 +1,12 @@
 package fr.ensim.interop.introrest.controller;
 
+import fr.ensim.interop.introrest.model.telegram.ApiResponseUpdateTelegram;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class MessageRestController {
@@ -10,4 +15,14 @@ public class MessageRestController {
 	private String telegramApiUrl;
 	
 	//Opérations sur la ressource Message
+	@GetMapping(value = "/getUpdates")
+	public ResponseEntity<ApiResponseUpdateTelegram> getUpdates() {
+
+		RestTemplate restTemplate = new RestTemplate();
+		ApiResponseUpdateTelegram response = restTemplate.getForObject(telegramApiUrl+"/getUpdates",
+				ApiResponseUpdateTelegram.class);
+
+		return ResponseEntity.ok().body(response);
+	}
+
 }
