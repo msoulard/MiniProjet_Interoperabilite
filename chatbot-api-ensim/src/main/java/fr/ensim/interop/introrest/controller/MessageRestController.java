d2b309a215ed97a306b4c61ff5090841ffc0b691
msoulard <soulardmimi@gmail.com>
2023-04-12 12:22:31 +0200

    méthode sendMessage
    méthode dans classe MessageRestController
    méthode fonctionnelle

--- chatbot-api-ensim/src/main/java/fr/ensim/interop/introrest/controller/MessageRestController.java
+++ chatbot-api-ensim/src/main/java/fr/ensim/interop/introrest/controller/MessageRestController.java
@@ -1,11 +1,10 @@
 package fr.ensim.interop.introrest.controller;
 
-import fr.ensim.interop.introrest.model.telegram.ApiResponseUpdateTelegram;
+import fr.ensim.interop.introrest.model.telegram.*;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
@@ -13,6 +12,8 @@ public class MessageRestController {
 	
 	@Value("${telegram.api.url}")
 	private String telegramApiUrl;
+	@Value("${telegram.chat.id}")
+	private String chatId;
 	
 	//Opérations sur la ressource Message
 	@GetMapping(value = "/getUpdates")
@@ -21,6 +22,20 @@ public class MessageRestController {
 		RestTemplate restTemplate = new RestTemplate();
 		ApiResponseUpdateTelegram response = restTemplate.getForObject(telegramApiUrl+"/getUpdates",
 				ApiResponseUpdateTelegram.class);
+
+		return ResponseEntity.ok().body(response);
+	}
+
+	@PostMapping("/message")
+	public ResponseEntity<ApiResponseTelegram> sendMessage(@RequestBody MessageApi message) {
+		// le text du message obligatoire, non vide, non blanc)
+		if(!StringUtils.hasText(message.getText())) {
+			return ResponseEntity.badRequest().build();
+		}
+		message.setChat_id(Long.parseLong(chatId));
+		RestTemplate restTemplate = new RestTemplate();
+		ApiResponseTelegram response = restTemplate.postForObject(telegramApiUrl+"/sendMessage",message,
+				ApiResponseTelegram.class);
 
 		return ResponseEntity.ok().body(response);
 	}
