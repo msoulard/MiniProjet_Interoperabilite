5ba0f205b844486b35ae067ab8e23cf944b0f1d5
msoulard <soulardmimi@gmail.com>
2023-04-17 12:18:25 +0200

    polling
    voir pourquoi le traitement des messages ne se fait qu'au lancement de l'application

--- chatbot-api-ensim/src/main/java/fr/ensim/interop/introrest/ListenerUpdateTelegram.java
+++ chatbot-api-ensim/src/main/java/fr/ensim/interop/introrest/ListenerUpdateTelegram.java
@@ -12,9 +12,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -23,59 +20,56 @@ public class ListenerUpdateTelegram implements CommandLineRunner {
 
 	private static final String COMMAND_METEO = "meteo";
 	private static final String COMMAND_BLAGUE = "blague";
+	private Integer offset = 933058519;
+	private int offsetBis = offset.intValue();
 	@Autowired
 	private MessageRestController controller;
-	private ScheduledExecutorService executor;
 
 	@Override
 	public void run(String... args) throws Exception {
 		Logger.getLogger("ListenerUpdateTelegram").log(Level.INFO, "Démarage du listener d'updates Telegram...");
-		executor = Executors.newSingleThreadScheduledExecutor();
-		executor.scheduleAtFixedRate(this::pollUpdates, 0, 5, TimeUnit.SECONDS);
-	}
-	private Integer offset = 933058503;
-	private int offsetBis = offset.intValue();
-	private void pollUpdates() {
+		TimerTask task = new TimerTask() {
+			public void run() {
+				List<String> messagesATraiter = new ArrayList<>();
+				ResponseEntity<ApiResponseUpdateTelegram> reponseGet = controller.getUpdates(offsetBis);
+				offset = reponseGet.getBody().getResult().get(0).getUpdateId();
+				offsetBis = offset.intValue();
+				for (int i = 0; i < reponseGet.getBody().getResult().size(); i++) {
+					messagesATraiter.add(reponseGet.getBody().getResult().get(i).getMessage().getText());
+				}
 
-		List<String> messagesATraiter = new ArrayList<>();
-		ResponseEntity<ApiResponseUpdateTelegram> reponseGet = controller.getUpdates(offsetBis);
-		//if (reponseGet.getBody().getResult().size() > 0) {
-		offset = reponseGet.getBody().getResult().get(0).getUpdateId();
-		offsetBis = offset.intValue();
-
-		/*} else {
-			offset = null;
-		}*/
-		for (int i = 0; i < reponseGet.getBody().getResult().size(); i++) {
-			messagesATraiter.add(reponseGet.getBody().getResult().get(i).getMessage().getText());
-		}
-
-		while (!messagesATraiter.isEmpty()) {
-			String command = messagesATraiter.get(0);
-			Logger.getLogger("ListenerUpdateTelegram").log(Level.INFO, "message à traiter = "+ command);
-			switch (command) {
-				case COMMAND_METEO:
-					Logger.getLogger("ListenerUpdateTelegram").log(Level.INFO, "meteo");
-					controller.sendMessage(new MessageApi(0L, "resultat météo"));
-					offsetBis++;
-					messagesATraiter.remove(0);
-					break;
-				case COMMAND_BLAGUE:
-					Logger.getLogger("ListenerUpdateTelegram").log(Level.INFO, "blague");
-					controller.sendMessage(new MessageApi(0L, "resultat blague"));
-					offsetBis++;
-					messagesATraiter.remove(0);
-					break;
-				default:
-					Logger.getLogger("ListenerUpdateTelegram").log(Level.INFO, "default");
-					controller.sendMessage(new MessageApi(0L, "bonjour"));
-					offsetBis++;
-					messagesATraiter.remove(0);
-					break;
+				while (!messagesATraiter.isEmpty()) {
+					String command = messagesATraiter.get(0);
+					Logger.getLogger("ListenerUpdateTelegram").log(Level.INFO, "message à traiter = "+ command);
+					switch (command) {
+						case COMMAND_METEO:
+							Logger.getLogger("ListenerUpdateTelegram").log(Level.INFO, "meteo");
+							controller.sendMessage(new MessageApi(0L, "resultat météo"));
+							offsetBis++;
+							messagesATraiter.remove(0);
+							break;
+						case COMMAND_BLAGUE:
+							Logger.getLogger("ListenerUpdateTelegram").log(Level.INFO, "blague");
+							controller.sendMessage(new MessageApi(0L, "resultat blague"));
+							offsetBis++;
+							messagesATraiter.remove(0);
+							break;
+						default:
+							Logger.getLogger("ListenerUpdateTelegram").log(Level.INFO, "default");
+							controller.sendMessage(new MessageApi(0L, "bonjour"));
+							offsetBis++;
+							messagesATraiter.remove(0);
+							break;
+					}
+					Logger.getLogger("ListenerUpdateTelegram").log(Level.INFO, "offset = "+offsetBis);
+				}
+				messagesATraiter.clear();
 			}
-			Logger.getLogger("ListenerUpdateTelegram").log(Level.INFO, "offset = "+offsetBis);
-		}
-		messagesATraiter.clear();
-		//offsetBis--;
+		};
+		Timer timer = new Timer("Timer");
+
+		long delay = 10L;
+		timer.schedule(task, delay);
 	}
+
 }
--- chatbot-api-ensim/src/main/java/fr/ensim/interop/introrest/controller/MessageRestController.java
+++ chatbot-api-ensim/src/main/java/fr/ensim/interop/introrest/controller/MessageRestController.java
@@ -15,8 +15,6 @@ public class MessageRestController {
 	private String telegramApiUrl;
 	@Value("${telegram.chat.id}")
 	private String chatId;
-	//private Integer offset;
-	//private List<String> messagesATraiter;
 	
 	//Opérations sur la ressource Message
 	@GetMapping(value = "/getUpdates")
@@ -32,10 +30,6 @@ public class MessageRestController {
 			response = restTemplate.getForObject(telegramApiUrl + "/getUpdates",
 					ApiResponseUpdateTelegram.class);
 		}
-		/*offset = response.getResult().get(0).getUpdateId();
-		for(int i = 0 ; i < response.getResult().size()-1 ; i++) {
-			messagesATraiter.add(response.getResult().get(i).getMessage().getText());
-		}*/
 
 		return ResponseEntity.ok().body(response);
 	}
@@ -48,46 +42,8 @@ public class MessageRestController {
 		}
 		message.setChat_id(Long.parseLong(chatId));
 		RestTemplate restTemplate = new RestTemplate();
-		//ApiResponseTelegram response;
 		ApiResponseTelegram response = restTemplate.postForObject(telegramApiUrl+"/sendMessage",message,
 				ApiResponseTelegram.class);;
-		/*switch (messagesATraiter.get(0)){
-			case "meteo" :
-				if(messagesATraiter.size() == 0){
-					messagesATraiter.remove(0);
-					message.setText("resultat météo");
-					response = restTemplate.postForObject(telegramApiUrl+"/sendMessage",message,
-							ApiResponseTelegram.class);
-					offset++;
-				}
-				else{
-					offset++;
-				}
-				break;
-			case "blaque" :
-				if(messagesATraiter.size() == 0){
-					messagesATraiter.remove(0);
-					message.setText("resultat blague");
-					response = restTemplate.postForObject(telegramApiUrl+"/sendMessage",message,
-							ApiResponseTelegram.class);
-					offset++;
-				}
-				else{
-					offset++;
-				}
-				break;
-			default :
-				if(messagesATraiter.size() == 0){
-					messagesATraiter.remove(0);
-					response = restTemplate.postForObject(telegramApiUrl+"/sendMessage",message,
-							ApiResponseTelegram.class);
-					offset++;
-				}
-				else{
-					offset++;
-				}
-				break;
-		}*/
 
 		return ResponseEntity.ok().body(response);
 	}
