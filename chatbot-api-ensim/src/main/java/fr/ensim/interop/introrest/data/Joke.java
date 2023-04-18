1da97ee175afedc4d1ed2efc6ce5bf20220de9a5
Ouijdane IMER <67414126+Ouijdane-imer@users.noreply.github.com>
2023-04-18 12:24:31 +0200

    EnregistrementNoteDansJson

--- chatbot-api-ensim/src/main/java/fr/ensim/interop/introrest/data/Joke.java
+++ chatbot-api-ensim/src/main/java/fr/ensim/interop/introrest/data/Joke.java
@@ -1,6 +1,12 @@
 package fr.ensim.interop.introrest.data;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.core.io.ClassPathResource;
+
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class Joke {
     private String titre;
@@ -43,6 +49,16 @@ public class Joke {
     public void setNote(int note)
     {
         this.note=note;
+    }
+
+    public void ChangeNoteBlague(int _note) throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        ClassPathResource classPathResource = new ClassPathResource("Joke.json");
+        InputStream inputStream = classPathResource.getInputStream();
+        JsonNode jsonNode = objectMapper.readTree(inputStream);
+        int noteChange = (note+_note)/2;
+        objectMapper.writeValue(new File(classPathResource.getFile().toURI()), noteChange);
+
     }
 }
 
